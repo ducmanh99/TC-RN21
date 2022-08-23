081852da3c5cb2dde22cb1f90549eeaf34522563
ducmanh99 <pdm241099@gmail.com>
2022-08-23 23:06:22 +0700

    file

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,25 +1,25 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-let { connectDB } = require("./../helper/connect")
+let { connectDB } = require("./../helper/connect");
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  connectDB().then(client => {
-  console.log("hihi")
+router.get("/", function (req, res, next) {
+	connectDB()
+		.then((client) => {
+			console.log("hihi");
 
-    client.collection("users").find({}).toArray( (err, result) => {
-      console.log(result)
-      res.status(200).json({
-        message: "Hello Stupid!",
-        result: result
-      })
-    })
-
-  }).catch(next)
-
-
+			client
+				.collection("users")
+				.find({})
+				.toArray((err, result) => {
+					console.log(result);
+					res.status(200).json({
+						message: "Hello My Friend",
+						result: result,
+					});
+				});
+		})
+		.catch(next);
 });
-
-
 
 module.exports = router;
